c9d2dab5250f063bf4c0ca0806eff13ff4731da5
asukabaka <48495833+asukabaka@users.noreply.github.com>
2022-03-16 21:10:07 -0700

    first commit
    two pages to test swup and prepare for twitter bootstrap integration

---
+++ js/swup-init.js
@@ -0,0 +1,15 @@
+
+
+const swup = new Swup(
+{
+  plugins: [new SwupProgressPlugin(
+  {
+  className: 'swup-progress-bar',
+  transition: 800,
+  delay: 0
+}
+)]
+  
+
+}
+); // only this line when included with script tag
